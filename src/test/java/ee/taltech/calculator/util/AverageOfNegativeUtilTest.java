d2e775eb337a4f8eb2810bfbb40fcb2bd019d842
cacagl <cacagl@ttu.ee>
2022-03-06 20:23:40 +0200

    added tests for average of negatives and even

---
+++ src/test/java/ee/taltech/calculator/util/AverageOfNegativeUtilTest.java
@@ -0,0 +1,29 @@
+package ee.taltech.calculator.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AverageOfNegativeUtilTest
+{
+
+    @Test
+    @DisplayName("Average result is empty or null")
+    void ResultNullOrEmpty()
+    {
+        assertNull(AverageOfNegativeUtil.averageOfNegative(null));
+        assertNull(AverageOfNegativeUtil.averageOfNegative(List.of()));
+    }
+
+    @Test
+    @DisplayName("Average of negatives is correct")
+    void resultIsCorrect()
+    {
+        assertEquals(-4, AverageOfNegativeUtil.averageOfNegative(List.of(-2,-6)));
+        assertEquals(-10, AverageOfNegativeUtil.averageOfNegative(List.of(2,4,6,8,-12,-14,-8,-6)));
+    }
+}
+
---
+++ src/test/java/ee/taltech/calculator/util/EvenCalculatorUtilTest.java
@@ -0,0 +1,26 @@
+package ee.taltech.calculator.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EvenCalculatorUtilTest {
+
+    @Test
+    @DisplayName("Even result is null or empty")
+    void evenResultNullOrEmpty(){
+        assertNull(EvenCalculatorUtil.evenNumbers(null));
+        assertNull(EvenCalculatorUtil.evenNumbers(List.of()));
+
+    }
+
+    @Test
+    @DisplayName("Even result is correct")
+    void evenResultTest(){
+        assertEquals(List.of(6,12), EvenCalculatorUtil.evenNumbers(List.of(7, 6, 12, 73, 1, 3)));
+        assertEquals(List.of(118, 224), EvenCalculatorUtil.evenNumbers(List.of(224,443,23953,118,118)));
+    }
+}
